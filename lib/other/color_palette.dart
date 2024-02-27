ba464a6842158d315ef5d4b7a09ed761562cc4c2
NikthePhoenix <nikgov23@gmail.com>
2024-02-27 10:38:20 +0530

    added styling for bottom navigation bar

--- lib/other/color_palette.dart
+++ lib/other/color_palette.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 const object_bg_darkblue=0xff012e65;
 const object_bg_lightblue=0xff61c5ff;
 const text_dm_offwhite=0xffecffd1;
@@ -5,3 +7,4 @@ const golden_yellow=0xffe1a730;
 const shadow_blue=0xff3c7bc2;
 const background_lightblue=0xffabe0ff;
 const background_darkgrey=0xff161616;
+const selected_maroon=0xff650000;
